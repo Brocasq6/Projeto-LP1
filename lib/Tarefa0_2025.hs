600d91dfd11b35840f500f991020bf14b5124716
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-10-10 22:13:41 +0100

    Atualiza Tarefa0_2025.hs com novas alterações nas funcoes destroiPosicao / minhocatTemDisparo / ePosicaoEstadoLivre

--- lib/Tarefa0_2025.hs
+++ lib/Tarefa0_2025.hs
@@ -22,6 +22,15 @@ minhocaExemplo = Minhoca
     , dinamiteMinhoca = 0
 }
 
+
+mapaExemplo :: [[Terreno]]
+mapaExemplo =
+    [ [Terra, Agua, Agua]
+    , [Terra, Terra, Terra]
+    , [Pedra, Pedra, Pedra]
+    ]
+    
+
 -- | Retorna a quantidade de munições disponíveis de uma minhoca para uma dada arma.
 encontraQuantidadeArmaMinhoca :: TipoArma -> Minhoca -> Int
 encontraQuantidadeArmaMinhoca Jetpack minhoca = jetpackMinhoca minhoca 
@@ -34,11 +43,11 @@ encontraQuantidadeArmaMinhoca Dinamite minhoca = dinamiteMinhoca minhoca
 atualizaQuantidadeArmaMinhoca :: TipoArma -> Minhoca -> Int -> Minhoca
 atualizaQuantidadeArmaMinhoca arma minhoca novasBalas =
     case arma of
-        Jetpack -> minhoca {jetpackMinhoca = jetpackMinhoca + novasBalas} -- atualizo apenas o campo da municao da arma que estou a usar para a nova quantidade de balas?
-        Escavadora -> minhoca {escavadoraMinhoca = escavadoraMinhoca + novasBalas}
-        Bazuca -> minhoca {bazucaMinhoca = bazucaMinhoca + novasBalas}
-        Mina -> minhoca {minaMinhoca = minaMinhoca + novasBalas}
-        Dinamite -> minhoca {dinamiteMinhoca = dinamiteMinhoca + novasBalas}
+        Jetpack -> minhoca { jetpackMinhoca = jetpackMinhoca minhoca + novasBalas } -- atualizo apenas o campo da municao da arma que estou a usar para a nova quantidade de balas?
+        Escavadora -> minhoca { escavadoraMinhoca = escavadoraMinhoca minhoca + novasBalas }
+        Bazuca -> minhoca { bazucaMinhoca = bazucaMinhoca minhoca + novasBalas }
+        Mina -> minhoca { minaMinhoca = minaMinhoca minhoca + novasBalas }
+        Dinamite -> minhoca { dinamiteMinhoca = dinamiteMinhoca minhoca + novasBalas }
 
 
 -- | Verifica se um tipo de terreno é destrutível, i.e., pode ser destruído por explosões.
@@ -67,8 +76,9 @@ eTerrenoOpaco terreno =
 ePosicaoMapaLivre :: Posicao -> Mapa -> Bool
 ePosicaoMapaLivre pos mapa =  
     case encontraPosicaoMatriz pos mapa of
-        Just terreno -> not (eTerrenoOpaco terreno)
-        Nothing      -> False -- heloo
+        Just terreno -> not (eTerrenoOpaco terreno)     -- se a posicao for Agua ou Ar, retorna True
+        Nothing      -> False                           -- se a posicao for terra ou pedra, retorna False
+
 -- vamos ter de verificar se a posicao existe no mapa, e se o terreno nessa posicao é opaco ou nao
 
 -- | Verifica se uma posição do estado está livre, i.e., pode ser ocupada por um objeto ou minhoca.
@@ -76,18 +86,29 @@ ePosicaoMapaLivre pos mapa =
 -- __NB:__ Uma posição está livre se o mapa estiver livre e se não estiver já uma minhoca ou um barril nessa posição.
 
 ePosicaoEstadoLivre :: Posicao -> Estado -> Bool
-ePosicaoEstadoLivre = undefined
+ePosicaoEstadoLivre pos = undefined
+
+
+-- funcao que verifica se a posicao se encontra livre de minhicas
+
+-- funcao que verifica se a posicao se encontra livre de barris
+
 
 -- | Verifica se numa lista de objetos já existe um disparo feito para uma dada arma por uma dada minhoca.
 minhocaTemDisparo :: TipoArma -> NumMinhoca -> [Objeto] -> Bool 
-minhocaTemDisparo = undefined
+minhocaTemDisparo _ _ [] = False -- se a lista de objetos estiver vazia, retorna False
+minhocaTemDisparo arma num (objeto:objetos) = undefined
+
 -- vamos ter de verificar se o objeto é um disparo, e se o disparo é da arma e da minhoca que estamos a procurar
 
 -- | Destrói uma dada posição no mapa (tipicamente como consequência de uma explosão).
 --
 -- __NB__: Só terrenos @Terra@ pode ser destruídos.
 destroiPosicao :: Posicao -> Mapa -> Mapa -- vamos ter de verificar se o terreno é destrutivel, se for, atualizamos a posicao para Ar
-destroiPosicao = undefined
+destroiPosicao pos mapa = 
+    case encontraPosicaoMatriz pos mapa of
+        Just Terra -> atualizaPosicaoMatriz pos Ar mapa  
+        Nothing -> mapa
 
 -- Adiciona um novo objeto a um estado.
 --
