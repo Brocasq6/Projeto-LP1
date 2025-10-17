e060265837bebf4dab7345ae1ecaebe348358311
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-10-17 19:31:57 +0100

    adicionei um teste

--- test/Tarefa1Test.hs
+++ test/Tarefa1Test.hs
@@ -6,14 +6,35 @@ import Magic
 
 -- | Definir aqui os testes do grupo para a Tarefa 1
 testesTarefa1 :: [Estado]
-testesTarefa1 = [estadoExemplo1]
+testesTarefa1 = [estadoExemplo2]
 
-estadoExemplo1 :: Estado
-estadoExemplo1 =
+estadoExemplo2 :: Estado
+estadoExemplo2 =
   Estado
-    { mapaEstado = replicate 3 (replicate 3 Terra)
-    , objetosEstado = [Barril (2,2) False]
-    , minhocasEstado = [Minhoca (Just (1,1)) (Viva 100) 1 1 1 1 1]
+    { mapaEstado =
+        replicate 10 (replicate 20 Ar)
+    , objetosEstado =
+        [Barril (2,2) False]  -- barril na mesma posição da segunda minhoca
+    , minhocasEstado =
+        [ Minhoca
+            { posicaoMinhoca = Just (0,0)
+            , vidaMinhoca = Viva 100
+            , jetpackMinhoca = 2
+            , escavadoraMinhoca = 1
+            , bazucaMinhoca = 3
+            , minaMinhoca = 1
+            , dinamiteMinhoca = 0
+            }
+        , Minhoca
+            { posicaoMinhoca = Just (2,2)
+            , vidaMinhoca = Viva 100
+            , jetpackMinhoca = 1
+            , escavadoraMinhoca = 1
+            , bazucaMinhoca = 1
+            , minaMinhoca = 1
+            , dinamiteMinhoca = 1
+            }
+        ]
     }
 
 dataTarefa1 :: IO TaskData
