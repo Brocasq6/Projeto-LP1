21ba6f955369b366a632b6208e9c48f72dd7a330
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-10-17 19:28:24 +0100

    adicionei um teste

--- test/Tarefa1Test.hs
+++ test/Tarefa1Test.hs
@@ -6,7 +6,15 @@ import Magic
 
 -- | Definir aqui os testes do grupo para a Tarefa 1
 testesTarefa1 :: [Estado]
-testesTarefa1 = []
+testesTarefa1 = [estadoExemplo1]
+
+estadoExemplo1 :: Estado
+estadoExemplo1 =
+  Estado
+    { mapaEstado = replicate 3 (replicate 3 Terra)
+    , objetosEstado = [Barril (2,2) False]
+    , minhocasEstado = [Minhoca (Just (1,1)) (Viva 100) 1 1 1 1 1]
+    }
 
 dataTarefa1 :: IO TaskData
 dataTarefa1 = do
