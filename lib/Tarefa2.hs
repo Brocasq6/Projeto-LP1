da40b497f314fa3bc964df5fc90385cc741e90ee
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-10-19 17:50:39 +0100

    adicionei os tipos e as funcoes que precisamos para fazer a funcao efetuaJogada

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -11,21 +11,58 @@ import Labs2025
 {-
 
 efetuaJogada
-  ── efetuaJogadaMove
- │      ── moveMinhoca
- │     │      ── proximaPosicao
- │     │      ── terrenoNaPosicao
- │     │      ── aplicaEfeitoTerreno
- │      ── atualizaLista
+ |
+ | ── efetuaJogadaMove
+ │      |── moveMinhoca
+ │      │       |── proximaPosicao
+ │      │       |── terrenoNaPosicao
+ │      │       |── aplicaEfeitoTerreno
+ │      |── atualizaLista
  │
-  ── efetuaJogadaDispara
-       ── temMunicao
-       ── consomeMunicao
-       ── criaDisparo
-       ── atualizaLista
+  ── efetuaJogadaDisparo
+        |── temMunicao
+        |── consomeMunicao
+        |── criaDisparo
+        |── atualizaLista
 
 -}
 
+
+--------------------------------------- funcoes relacionadas com a funcao efetuaJogadaMove -------------------------------------------------
+
+moveMinhoca :: Direcao -> Mapa -> Minhoca -> Minhoca
+moveMinhoca dir mapa minhoca = undefined
+
+proximaPosicao :: Direcao -> Posicao -> Posicao
+proximaPosicao dir (x,y) = undefined
+
+terrenoNaPosicao :: Mapa -> Posicao -> Terreno
+terrenoNaPosicao = undefined
+
+aplicaEfeitoTerreno :: Minhoca -> Posicao -> Terreno -> Minhoca
+aplicaEfeitoTerreno minhoca pos terreno = undefined
+
+efetuaJogadaMove :: NumMinhoca -> Direcao -> Estado -> Estado
+efetuaJogadaMove n dir est = undefined
+
+--------------------------------------- funcoes relacionadas com a funcao efetuaJogadaDisparo -------------------------------------------------
+
+temMunicao :: TipoArma -> Bool
+temMunicao arma mun = undefined
+
+consomeMunicao :: TipoArma -> Minhoca -> Minhoca
+consomeMunicao arma minhoca = undefined
+
+criaDisparo :: TipoArma -> Direcao -> NumMinhoca -> Minhoca -> Objeto
+criaDisparo arma dir dono minhoca = undefined
+
+atualizaLista :: Int -> a -> [a] -> [a]
+atualizaLista i novo l = undefined 
+
+efetuaJogadaDisparo :: NumMinhoca -> TipoArma -> Direcao -> Estado -> Estado
+efetuaJogadaDisparo n arma dir estado = undefined 
+
+----------------------------------------------------------------------------------------------------------------
 
 -- | Função principal da Tarefa 2. Recebe o índice de uma minhoca na lista de minhocas, uma jogada, um estado e retorna um novo estado em que essa minhoca efetuou essa jogada.
 efetuaJogada :: NumMinhoca -> Jogada -> Estado -> Estado
