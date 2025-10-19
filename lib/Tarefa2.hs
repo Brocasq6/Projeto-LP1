6a03c6133942bd2e7bc01e919aefe77347ba1dcd
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-10-19 17:29:17 +0100

    esquema das funcoes que precisam de ser feitas para concluir a funcao efetuaJoagada e estrutura da função efetuaJogada

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -8,7 +8,29 @@ module Tarefa2 where
 
 import Labs2025
 
+{-
+
+efetuaJogada
+  ── efetuaJogadaMove
+ │      ── moveMinhoca
+ │     │      ── proximaPosicao
+ │     │      ── terrenoNaPosicao
+ │     │      ── aplicaEfeitoTerreno
+ │      ── atualizaLista
+ │
+  ── efetuaJogadaDispara
+       ── temMunicao
+       ── consomeMunicao
+       ── criaDisparo
+       ── atualizaLista
+
+-}
+
+
 -- | Função principal da Tarefa 2. Recebe o índice de uma minhoca na lista de minhocas, uma jogada, um estado e retorna um novo estado em que essa minhoca efetuou essa jogada.
 efetuaJogada :: NumMinhoca -> Jogada -> Estado -> Estado
-efetuaJogada n j e = undefined
+efetuaJogada n jogada estado =
+  case jogada of
+    Move dir -> efetuaJogadaMove n dir estado
+    Dispara arma dir -> efetuaJogadaDispara n arma dir estado
 
