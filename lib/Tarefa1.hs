f776247bf4f262cce21f74880e6fbdad4c0b034a
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-10-24 14:15:04 +0100

    funcao terrenoNaPosicao atualizada

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -81,7 +81,7 @@ dentroMapa (x,y) mapa =
 -- Obtém o terreno existente numa posição (se for válida)
 terrenoNaPosicao :: Posicao -> Mapa -> Maybe Terreno
 terrenoNaPosicao (x,y) [] = Nothing
-terrenoNaPosicao (x,y) mapa =
+terrenoNaPosicao (x,y) mapa 
     | dentroMapa (x,y) mapa = Just ((mapa!!y)!!x) 
     | otherwise = Nothing
 
