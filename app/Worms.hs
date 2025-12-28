050e1c8c5e516ce6025fcf47b966147f45e638e0
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-12-28 20:51:33 +0000

    mapa mental do que e preciso fazer

--- app/Worms.hs
+++ app/Worms.hs
@@ -1,4 +1,26 @@
 module Worms where
 
+{-
+Worms.hs
+ |- data Worms = Worms
+ |    { jogo
+ |    , selW 
+ |    , selJ 
+ |    , ... (opcional: dtAcc, camera, mensagem)
+ |    }
+ |
+ | data SelJogada
+ |    = ... (lista de ações/armas)
+ |
+ |- nextSelJogada 
+ |- prevSelJogada 
+ |
+ |- defaultWorms 
+ |    └─ cria World a partir de um Estado inicial
+ |
+ |- wormsValidas                     
+ |- cycleSelW                   
+-}
+
 -- | Estado do jogo no Gloss.
 data Worms = Worms {}
