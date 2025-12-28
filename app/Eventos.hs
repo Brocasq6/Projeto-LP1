6351364e2dadb49e8f56a672b2f7693b690e13e7
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-12-28 20:31:28 +0000

    mapa mental daquilo que é preciso fazer

--- app/Eventos.hs
+++ app/Eventos.hs
@@ -3,6 +3,32 @@ module Eventos where
 import Graphics.Gloss.Interface.Pure.Game
 
 import Worms
+{-
+Eventos.hs
+ |- reageEventos :: Event -> Worms -> Worms
+ │    |- (tecla '1')  -> selecionaMinhocaSeguinte
+ │    |- (tecla '2')  -> selecionaJogadaSeguinte
+ │    |- (W/A/S/D)    -> aplicaJogadaDirecional
+ │    |- (outras)     -> mantém estado
+ │
+ |- selecionaMinhocaSeguinte :: Worms -> Worms
+ |    |- wormsValidas :: Estado -> [Int]
+ |    |- cycleSelW    :: Estado -> Int -> Int
+ |
+ |- selecionaJogadaSeguinte :: Worms -> Worms
+ |    |- nextSelJogada :: SelJogada -> SelJogada
+ |
+ |- aplicaJogadaDirecional :: Char -> Worms -> Worms
+ |    |- dirFromKey     :: Char -> Maybe Direcao
+ |    |- jogadaFromSel  :: SelJogada -> Direcao -> Jogada
+ |    |- aplicaEfetua   :: Int -> Jogada -> Estado -> Estado
+ |         |- efetuaJogada (T2)
+ |
+ |- dirFromKey :: Char -> Maybe Direcao
+ |- jogadaFromSel :: SelJogada -> Direcao -> Jogada
+ |- wormsValidas :: Estado -> [Int]
+
+-}
 
 -- | Função que altera o estado do jogo no Gloss.
 reageEventos :: Event -> Worms -> Worms
