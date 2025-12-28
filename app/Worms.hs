1ab3ee6f4af6a31d974cfc1fd2edc4bef13c1071
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-12-28 21:02:47 +0000

    inicio do trabalho

--- app/Worms.hs
+++ app/Worms.hs
@@ -23,4 +23,23 @@ Worms.hs
 -}
 
 -- | Estado do jogo no Gloss.
-data Worms = Worms {}
+data Worms = Worms 
+    { jogo
+    , selW 
+    , selJ 
+    }
+
+nextSelJogada :: SelJogada -> SelJogada
+nextSelJogada j = undefined
+
+prevSelJogada :: SelJogada -> SelJogada
+prevSelJogada j = undefined
+
+defaultWorms :: Estado -> Worms
+defaultWorms e = undefined
+
+wormsValida :: Estado -> [Int]
+wormsValida e = undefined
+
+cycleSel :: Estado -> Int -> Int
+cycleSel e = undefined
