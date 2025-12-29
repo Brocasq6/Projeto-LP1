9c64573d16a9c19dcee8a10d0ce68d011287defb
Bruno Bonifácio <a110016@alunos.uminho.pt>
2025-12-29 14:41:29 +0000

    mapa mental do que é necessario fazer para o ficheiro Desenhar.hs

--- app/Desenhar.hs
+++ app/Desenhar.hs
@@ -4,6 +4,14 @@ import Graphics.Gloss
 
 import Worms
 
+{-
+    desenha
+     |- desenhaMapa
+     |- desenhaMinhocas
+     |- desenhaHUD
+
+-}
+
 -- | Função que desenha o estado do jogo no Gloss.
 desenha :: Worms -> Picture
 desenha _ = Translate (-450) 0 $ Scale 0.5 0.5 $ Text "Welcome to Worms!"
